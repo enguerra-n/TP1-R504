ae39700d4d61947e4ad73ac950d4ae3b2ccca3ba
enguerranecole <enguerran.lecoq@univ-rouen.fr>
2024-09-11 11:30:53 +0200

    fin seance

--- TP1/fonction.py
+++
@@ -1,5 +0,0 @@
-def puissance (a,b) : 
-    result = a**b
-    return result
-
-print(puissance(2,2))
--- TP1/test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import fonctions as f
-def test_1 ( ) :
-    assert f.puissance(2 ,3) == 8
-    assert f.puissance(2 ,2) == 4
-
-def test_2 ( ) :
-    assert f.puissance(-2 ,3) == -8
-    assert f.puissance(-2 ,2) == 4
