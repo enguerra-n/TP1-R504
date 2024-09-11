425528050228ce70166b85a30a69fa97c314074b
enguerranecole <enguerran.lecoq@univ-rouen.fr>
2024-09-11 11:17:51 +0200

    modification des scripts python

--- fonctions.py
+++ fonctions.py
@@ -1,8 +1,20 @@
-def puissance (a,b) : 
-    R=1
-    for i in range (0,b) :
-    	R = a*R
-    	print(R)
-    return R
+def puissance(base, exposant):
+    try :
+        if exposant == 0:
+            return 1
+        elif exposant < 0:
+            return 1 / puissance(base, -exposant)
+    
+        resultat = 1
+        base_temp = base
+
+        for i in range (exposant -1):
+            base_temp *= base
+
+        return base_temp
+    except ZeroDivisionError:
+        print("Attention, il n'est pas possible de diviser par 0 ! ")
+
+
 
 #print(puissance(2,2))
